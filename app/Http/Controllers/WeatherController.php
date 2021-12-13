74e4ebe4fbc8865adfe7806f3f7c65063c8482ed
Muhammad Zarar <zarar.awan001@gmail.com>
2021-12-13 15:18:50 +0500

    Installation of location and weather packages is done

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    
+
+    public function index(Request $request)
+    {
+           
+    }
+}
