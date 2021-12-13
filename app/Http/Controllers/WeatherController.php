732bf8b08763a588dd4dc23887f4da5e09ab25bb
Muhammad Zarar <zarar.awan001@gmail.com>
2021-12-13 15:36:08 +0500

    Everything is done

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Stevebauman\Location\Facades\Location;
-
+use RakibDevs\Weather\Weather;
 
 class WeatherController extends Controller
 {
@@ -13,12 +13,15 @@ class WeatherController extends Controller
 
     public function index(Request $request)
     {
-            // $ip = $request->ip();
-            $ip = '72.255.21.43';
+             $ip = $request->ip();
+            //dd($ip)
+            //$ip = '72.255.21.43';
         $data = Location::get($ip);
         $city=$data->cityName;
-        //dd($city);
+        $weather = new Weather();
+        $weatherDetails = $weather->getCurrentByCity($city);
+        $array = json_decode(json_encode($weatherDetails));
 
-        return view('welcome');
+        return view('welcome', ['array'=>$array]);
     }
 }
